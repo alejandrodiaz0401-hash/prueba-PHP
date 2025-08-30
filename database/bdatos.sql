7268afc9142915a4a16e62a4e8cd70be4aaf4c38
jose Alejandro <alejandrodiaz0401@gmail.com>
2025-08-29 20:37:53 -0500

    creacion de productos en base de datos

--- database/bdatos.sql
+++ database/bdatos.sql
@@ -14,3 +14,27 @@ CREATE TABLE IF NOT EXISTS t_usuarios (
 ) ENGINE=InnoDB;
 
 INSERT INTO t_usuarios VALUES (NULL, 'Alejandro', 'Diaz', 'alejandrodiaz0401@gmail.com', '123456', 'admin', 'imagen');
+
+CREATE TABLE IF NOT EXISTS t_categorias (
+    id_categoria         INT(11) AUTO_INCREMENT NOT NULL,
+    nombre_categoria     VARCHAR(100)           NOT NULL,
+   CONSTRAINT pk_categoria PRIMARY KEY (id_categoria)
+ 
+)ENGINE=InnoDB;-
+
+INSERT INTO t_categorias VALUE (NULL,'Construccion'),(NULL,'electricos'),(NULL,'piso' ).(NULL 'pintura');
+
+CREATE TABLE IF NOT EXISTS t_productos (
+    id_producto      INT AUTO_INCREMENT NOT NULL,
+    nombre_producto  VARCHAR(100)       NOT NULL,
+    Descripcion      VARCHAR            NOT NULL,
+    fecha            DATE               NOT NULL,
+    stock            INT                NOT NULL,
+    precio           DECIMAL(10,2)      NOT NULL,
+    imagen           VARCHAR(200),
+    oferta           VARCHAR(255)       NOT NULL,
+    id_categoria     INT                NOT NULL,
+    CONSTRAINT  pk_usuario             PRIMARY KEY (id_producto),
+    CONSTRAINT  fk_PRODUCTO             FOREIGN KEY (id_categoria) REFERENCES t_categorias(id_categoria)
+) ENGINE=InnoDB;
+
